4b2363d0f7d823d0d40542b57eab0042a2ff52d6
sufiyan khan <suffunew@gmail.com>
2026-01-08 22:41:27 +0530

    get element

---
+++ script.js
@@ -0,0 +1,9 @@
+const balance = document.getElementById("balance");
+const income = document.getElementById("income");
+const expense = document.getElementById("expense");
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+
+
