086d407d67764594dcc2fe7612151816a0f997ed
sufiyan khan <suffunew@gmail.com>
2026-01-09 21:49:27 +0530

    add transactions

--- script.js
+++ script.js
@@ -6,4 +6,30 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
+// Load transactions from localStorage
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value === "" || amount.value === "") {
+    alert("Please enter text and amount");
+    return;
+  }
+
+  const transaction = {
+    id: Date.now(),
+    text: text.value,
+    amount: +amount.value
+  };
+
+  transactions.push(transaction);
+
+  addTransactionDOM(transaction);
+  updateValues();
+  updateLocalStorage();
+
+  text.value = "";
+  amount.value = "";
+}
