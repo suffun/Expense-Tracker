e14b67bdfe119dc81d4db5a9dc818855f34da5d1
sufiyan khan <suffunew@gmail.com>
2026-01-12 21:45:55 +0530

    practicing again

--- script.js
+++ script.js
@@ -6,84 +6,19 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
-// Load transactions from localStorage
-let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
-// Add transaction
-function addTransaction(e) {
-  e.preventDefault();
+let transactions = [];
 
-  if (text.value === "" || amount.value === "") {
-    alert("Please enter text and amount");
-    return;
-  }
-
-  const transaction = {
-    id: Date.now(),
-    text: text.value,
-    amount: +amount.value
+function addTransaction(text,amount){
+  let transaction = {
+    text :text,
+    amount : amount,
   };
-
   transactions.push(transaction);
-
-  addTransactionDOM(transaction);
-  updateValues();
-  updateLocalStorage();
-
-  text.value = "";
-  amount.value = "";
+  console.log(transactions);
 }
 
-// Add transaction to DOM
-function addTransactionDOM(transaction) {
-  const sign = transaction.amount < 0 ? "-" : "+";
-
-  const item = document.createElement("li");
-  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
-
-  item.innerHTML = `
-    ${transaction.text}
-    <span>${sign}₹${Math.abs(transaction.amount)}</span>
-    <button onclick="removeTransaction(${transaction.id})">x</button>
-  `;
-
-  list.appendChild(item);
-}
-
-// Update balance, income, expense
-function updateValues() {
-  const amounts = transactions.map(t => t.amount);
-
-  const total = amounts.reduce((acc, val) => acc + val, 0);
-  const inc = amounts.filter(val => val > 0).reduce((acc, val) => acc + val, 0);
-  const exp = amounts.filter(val => val < 0).reduce((acc, val) => acc + val, 0);
-
-  balance.innerText = `₹${total}`;
-  income.innerText = `₹${inc}`;
-  expense.innerText = `₹${Math.abs(exp)}`;
-}
-
-// Remove transaction
-function removeTransaction(id) {
-  transactions = transactions.filter(t => t.id !== id);
-
-  updateLocalStorage();
-  init();
-}
-
-// Update localStorage
-function updateLocalStorage() {
-  localStorage.setItem("transactions", JSON.stringify(transactions));
-}
-
-// Initialize app
-function init() {
-  list.innerHTML = "";
-  transactions.forEach(addTransactionDOM);
-  updateValues();
-}
-
-form.addEventListener("submit", addTransaction);
-
-init();
-
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  console.log("form submitted");
+})
