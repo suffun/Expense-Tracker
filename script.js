3234a65942815f2cdb11b4157ab0a794f2c6844a
sufiyan khan <suffunew@gmail.com>
2026-01-13 19:44:43 +0530

    make agin simpler version

--- script.js
+++ script.js
@@ -7,18 +7,71 @@ const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
 
-let transactions = [];
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
-function addTransaction(text,amount){
+function addTransaction(textValue, amountValue) {
   let transaction = {
-    text :text,
-    amount : amount,
+    id: Date.now(),
+    text: textValue,
+    amount: amountValue
   };
   transactions.push(transaction);
-  console.log(transactions);
+
+  addTransactionDOM(transaction);
+  updateValues()
+
 }
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log("form submitted");
-})
+
+  const textValue = text.value;
+  const amountValue = amount.value;
+
+  addTransaction(text.value, +amount.value);
+
+});
+function addTransactionDOM(transaction) {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    ${transaction.text} 
+    <span>₹${Math.abs(transaction.amount)}</span>
+    <button onclick="removeTransaction(${transaction.id})">x</button>
+  `;
+  list.appendChild(li);
+
+}
+
+function updateValues() {
+  let total = 0;
+  transactions.forEach((t) => {
+    total += t.amount;
+  });
+  balance.innerText = total;
+
+  let incomeTotal = 0;
+  let expenseTotal = 0;
+
+  transactions.forEach((t) => {
+    if (t.amount > 0) {
+      incomeTotal += t.amount;
+    } else {
+      expenseTotal += t.amount;
+    }
+  });
+  income.innerText = incomeTotal;
+  expense.innerText = Math.abs(expenseTotal);
+}
+
+function removeTransaction(id) {
+  transactions = transactions.filter(t => t.id !== id);
+  init();
+}
+function init() {
+  list.innerHTML = "";
+
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+localStorage.setItem("transactions",JSON.stringify(transactions));
